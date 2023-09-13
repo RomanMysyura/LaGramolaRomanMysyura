3b27b6fc8b316c73794f41109cb42d36a4ed05b6
RomanMysyura <rmysyura@cendrassos.net>
2023-09-13 19:11:52 +0200

    Actualitzat

--- frontend/index.php
+++ frontend/index.php
@@ -7,33 +7,21 @@
     <title>Gramola MP3</title>
 </head>
 <body>
-    <div class="player">
+    <div class="reproductor">
+
+        <div class="logosongs"></div>
         <div class="art-box">
-            <img src="" alt="Cover Art" id="coverArt">
-            <div class="time-display">
-                <span id="currentTime">00:00</span> / <span id="duration">00:00</span>
-            </div>
-            <div class="volume-meter"><div id="volumeLevel"></div></div>
+            <h3 class="nomsongs">Billie Eilish - What Was I Made For?</h3>
         </div>
+
         <div class="controls-box">
-            <button id="prev" disabled>⏮</button>
-            <button id="play">▶️</button>
-            <button id="pause" style="display: none;">⏸️</button>
-            <button id="stop">⏹️</button>
-            <button id="next" disabled>⏭</button>
-            <button id="random">🔀</button>
-            <progress id="progressBar" value="0" max="100"></progress>
+
         </div>
+
         <div class="status-box">
-            <span id="statusMessage">Benvingut a la Gramola MP3 ROman Mysyura!</span>
+
         </div>
-    </div>
-    <div class="playlist-menu">
-        <!-- Els llistats de reproducció es carregaran aquí -->
-    </div>
 
-    <audio id="player"></audio>
-
-    <script src="scripts.js"></script>
+    </div>
 </body>
 </html>
