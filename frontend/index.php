442ad28f08d64a86006207ebe9b95fd3d1fa11be
RomanMysyura <rmysyura@cendrassos.net>
2023-09-14 16:06:40 +0200

    Updated up to 1.1.0

--- frontend/index.php
+++ frontend/index.php
@@ -4,14 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
+    <link rel="shortcut icon" href="images/jukeboxlogo.png">
     <title>Gramola MP3</title>
 </head>
 <body>
+<nav class="menu">
+    <ul>
+        <li><a href="index.php">Playlists</a></li>
+        <li><a href="infotecnica.php">Informació Tècnica</a></li>
+    </ul>
+</nav>
+
+
+
     <div class="reproductor">
 
-        <div class="logosongs"></div>
+        
+
         <div class="art-box">
-            <h3 class="nomsongs">Billie Eilish - What Was I Made For?</h3>
+            <div class="logosong">
+                <img src="images/BillieEilish1.png" class="iconasong" alt="Logo de la cancó" width="300" height="200">
+
+
+            </div>
+            <h3 class="nomsong">Billie Eilish - What Was I Made For?</h3>
         </div>
 
         <div class="controls-box">
@@ -21,6 +37,10 @@
         <div class="status-box">
 
         </div>
+
+    </div>
+    <div class="playlists">
+
 
     </div>
 </body>
---
+++ frontend/infotecnica.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="shortcut icon" href="images/jukeboxlogo.png">
+    <title>Informació Tècnica</title>
+</head>
+<body>
+<nav class="menu">
+    <ul>
+        <li><a href="index.php">Playlists</a></li>
+        <li><a href="infotecnica.php">Informació Tècnica</a></li>
+    </ul>
+</nav>
+
+</body>
+</html>
